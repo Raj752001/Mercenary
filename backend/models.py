2b758754313f39b657c1f7a26c92a5171785ffea
Raj Jikadra <rajjikadra123@gmail.com>
2022-02-20 13:32:50 +0530

    ForeignKey added to Models

--- backend/models.py
+++ backend/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Skill(models.Model):
+    id = models.UUIDField(primary_key=True)
+    name = models.CharField()
+
+
 class User(models.Model):
-    email = models.EmailField()
-    password = models.CharField()
+    email = models.EmailField(primary_key=True)
+    password = models.CharField(password=True)
     name = models.CharField()
     status = models.CharField() 
     profession = models.CharField()
     highest_education = models.CharField()
     college = models.CharField()
-    # skills = models.ForeignKey() # 1 to many
+    skills = models.ManyToManyField(Skill) # many to many
     about = models.TextField()
     contact_number = models.IntegerField()
     linkedin_link = models.URLField()
@@ -17,13 +22,8 @@ class User(models.Model):
     rank = models.IntegerField()
 
 
-class Skill(models.Model):
-    id = models.UUIDField()
-    name = models.CharField()
-
-
 class Mission(models.Model):
-    id = models.UUIDField()
+    id = models.UUIDField(primary_key=True)
     title = models.CharField()
     about = models.TextField()
     technologies = models.CharField()
@@ -36,20 +36,20 @@ class Mission(models.Model):
 
 
 class Members(models.Model):
-    project_id = models.UUIDField()
-    user_id = models.UUIDField()
+    mission_id = models.ForeignKey(Mission, primary_key=True, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, primary_key=True, on_delete=models.CASCADE)
     role = models.CharField()
 
 
 class Requests(models.Model):
-    project_id = models.UUIDField()
-    user_id = models.UUIDField()
+    mission_id = models.ForeignKey(Mission, primary_key=True, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, primary_key=True, on_delete=models.CASCADE)
     type = models.CharField()
     message = models.TextField()
 
 
 class Feedback(models.Model):
-    id = models.UUIDField()
+    id = models.UUIDField(primary_key=True)
     subject = models.CharField()
     description = models.TextField()
     image = models.ImageField() #1 to many 
